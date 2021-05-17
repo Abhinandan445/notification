8980c292f18f845c574f98bc8424ade2eb15344b
Abhinandan <abhinandant@imtac.com>
2021-05-17 12:00:34 +0530

    First commit

--- utilities/response_model.js
+++ utilities/response_model.js
@@ -1,5 +1,6 @@
 //@Functionality: Common API Response model
 class ResponseModel {
+
     //Need to take params from API's, as Input
     static show(success, data, message) {
         this.success = success;
